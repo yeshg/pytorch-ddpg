d7d699f5df7001d61cc40dc8486d81f67bbfdb6f
Yesh Godse <yesh.godse@gmail.com>
2019-07-11 17:29:15 -0700

    removed unnecessary function

--- rl_algos/utils/visdom_logging.py
+++ rl_algos/utils/visdom_logging.py
@@ -4,23 +4,6 @@ from visdom import Visdom
 
 import numpy as np
 
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
 class VisdomLinePlotter(object):
     """Plots to Visdom"""
     def __init__(self, env_name='main'):
